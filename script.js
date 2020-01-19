9579852bca96251eeacf8d8dd6ff79983738dd07
joondong2 <kwonjoondong@gmail.com>
2020-01-19 14:35:33 +0900

    키프리스 비기너 추가(적용x)

--- script.js
+++ script.js
@@ -135,6 +135,69 @@ function searchKipris(queryString, kr, tm) {
     document.body.removeChild(form);
 }
 
+function searchKipris_beginner(queryString, kr, tm) {
+    let form = document.createElement('form');
+
+    let objs = document.createElement('input');
+    objs.setAttribute('name', 'expression');
+    objs.setAttribute('value', queryString);
+    form.appendChild(objs);
+
+    objs = document.createElement('input');
+    objs.setAttribute('name', 'queryText');
+    objs.setAttribute('value', queryString.split('\"').join("&quot;"));
+    form.appendChild(objs);
+
+    if (!kr && !tm) {
+        objs = document.createElement('input');
+        objs.setAttribute('name', 'collectionValues');
+        objs.setAttribute('value', ['US_T.col', 'EP_T.col', 'WO_T.col', 'PAJ_T.col', 'CN_T.col', ]);
+        form.appendChild(objs);
+    }
+
+    form.setAttribute('method', 'post');
+    if (!tm) {
+        if (!kr) {
+            form.setAttribute('action', "http://abpat.kipris.or.kr/abpat/resulta.do");
+        } else {
+            objs = document.createElement('input');
+            objs.setAttribute('name', 'searchKeyword');
+            objs.setAttribute('value', queryString);
+            form.appendChild(objs);
+
+            objs = document.createElement('input');
+            objs.setAttribute('name', 'searchClass');
+            objs.setAttribute('value', 'firstSearch');
+            form.appendChild(objs);
+
+            objs = document.createElement('input');
+            objs.setAttribute('name', 'searchRight');
+            objs.setAttribute('value', 'patent');
+            form.appendChild(objs);
+
+            form.setAttribute('action', "http://beginner.kipris.or.kr/beginner/search/headerSearch.do");
+        }
+    } else {
+        if (!kr) {
+            form.setAttribute('method', 'get');
+            form.setAttribute('action', "http://abtm.kipris.or.kr/abtm/search/resultList.jsp");
+        } else {
+            form.setAttribute('action', "http://kdtj.kipris.or.kr/kdtj/searchLogina.do?method=loginTM&checkPot=Y");
+
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+
+    if (!kr && !tm) {
+        form.setAttribute('action', "http://abpat.kipris.or.kr/abpat/searchLogina.do?next=MainSearch");
+        form.submit();
+    }
+
+    document.body.removeChild(form);
+}
+
 //replaceAll prototype 선언
 String.prototype.replaceAll = function (org, dest) {
     return this.split(org).join(dest);
